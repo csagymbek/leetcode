6a8d7415faae2e7d0a92a7a8a6bf57035289c02a
csagymbek <csagymbek@gmail.com>
2024-02-04 23:42:56 -0600

    initial commit

---
+++ findLHS.js
@@ -0,0 +1,44 @@
+// 594. Longest Harmonious Subsequence
+// We define a harmonious array as an array where the difference between its maximum value and its minimum value is exactly 1.
+// Given an integer array nums, return the length of its longest harmonious subsequence among all its possible subsequences.
+// A subsequence of array is a sequence that can be derived from the array by deleting some or no elements without changing the order of the remaining elements.
+
+// Example 1:
+// Input: nums = [1,3,2,2,5,2,3,7]
+// Output: 5
+// Explanation: The longest harmonious subsequence is [3,2,2,2,3].
+
+// Example 2:
+// Input: nums = [1,2,3,4]
+// Output: 2
+
+// Example 3:
+// Input: nums = [1,1,1,1]
+// Output: 0
+
+// Constraints:
+// 1 <= nums.length <= 2 * 104
+// -109 <= nums[i] <= 109
+
+const findLHS = (nums) => {
+    let obj = {};
+    let max = 0;
+    for (let i = 0; i < nums.length; i++) {
+        obj[nums[i]] = (obj[nums[i]] || 0) + 1;
+    }
+    console.log(obj);
+    console.log(Object.keys(obj));
+    for (let key of Object.keys(obj)) {
+        console.log(key);
+        console.log(obj[key]);
+        console.log(obj.hasOwnProperty(key + 1));
+        if (obj[key - 1]) {
+            max = Math.max(max, obj[key] + obj[key - 1]);
+        }
+    }
+    return max;
+}
+
+console.log(findLHS([1, 3, 2, 2, 5, 2, 3, 7]));// 5
+console.log(findLHS([1, 2, 3, 4]));// 2
+console.log(findLHS([1, 1, 1, 1]));// 0
---
+++ longestSubarray.js
@@ -0,0 +1,43 @@
+// 1493. Longest Subarray of 1's After Deleting One Element
+// Given a binary array nums, you should delete one element from it.
+// Return the size of the longest non-empty subarray containing only 1's in the resulting array. Return 0 if there is no such subarray.
+
+// Example 1:
+// Input: nums = [1,1,0,1]
+// Output: 3
+// Explanation: After deleting the number in position 2, [1,1,1] contains 3 numbers with value of 1's.
+
+// Example 2:
+// Input: nums = [0,1,1,1,0,1,1,0,1]
+// Output: 5
+// Explanation: After deleting the number in position 4, [0,1,1,1,1,1,0,1] longest subarray with value of 1's is [1,1,1,1,1].
+
+// Example 3:
+// Input: nums = [1,1,1]
+// Output: 2
+// Explanation: You must delete one element.
+
+// Constraints:
+// 1 <= nums.length <= 105
+// nums[i] is either 0 or 1.
+
+var longestSubarray = function (nums) {
+    let i = 0;
+    let j = 0;
+    let k = 1;
+    while (i < nums.length) {
+        if (nums[i++] === 0) {
+            k--;
+        }
+        if (k < 0) {
+            if (nums[j++] === 0) {
+                k++;
+            }
+        }
+    }
+    return i - j;
+};
+
+console.log(longestSubarray([1, 1, 0, 1]));// 3
+console.log(longestSubarray([0, 1, 1, 1, 0, 1, 1, 0, 1]));// 5
+console.log(longestSubarray([1, 1, 1]));// 2
