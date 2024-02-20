6ddb07ef8f793ad4a5bf09319a782f70debf3297
csagymbek <csagymbek@gmail.com>
2024-02-20 13:59:35 -0600

    initial commit

--- pivotIndex.js
+++ pivotIndex.js
@@ -32,7 +32,19 @@
 
 
 var pivotIndex = function (nums) {
-
+    let total = nums.reduce((acc, val) => acc + val, 0);
+    console.log(total);
+    let leftSum = 0;
+    let rightSum = 0;
+    for (let i = 0; i < nums.length; i++) {
+        rightSum = total - nums[i] - leftSum;
+        console.log(rightSum);
+        if (leftSum === rightSum) {
+            return i;
+        }
+        leftSum += nums[i];
+    }
+    return -1;
 };
 
 console.log(pivotIndex([1, 7, 3, 6, 5, 6]));// 3
