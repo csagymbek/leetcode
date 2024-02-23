76f70ff28979dba84c8c9d3d75adfc24937a8f08
csagymbek <csagymbek@gmail.com>
2024-02-22 19:35:56 -0600

    initial commit

---
+++ maxSubArray.js
@@ -0,0 +1,49 @@
+// 53. Maximum Subarray
+// Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
+// Example 1:
+// Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+// Output: 6
+// Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+
+// Example 2:
+// Input: nums = [1]
+// Output: 1
+// Explanation: The subarray [1] has the largest sum 1.
+
+// Example 3:
+// Input: nums = [5,4,-1,7,8]
+// Output: 23
+// Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+
+// Constraints:
+// 1 <= nums.length <= 105
+// -104 <= nums[i] <= 104
+
+// Follow up: If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+
+// var maxSubArray = function (nums) {
+//     let curMax = nums[0];
+//     let max = nums[0];
+//     for (let i = 1; i < nums.length; i++) {
+//         curMax = Math.max(nums[i], curMax + nums[i]);
+//         max = Math.max(max, curMax);
+//     }
+//     return max;
+// };
+
+var maxSubArray = function (nums) {
+    let max = nums[0];
+    let curMax = 0;
+    for (let i = 0; i < nums.length; i++) {
+        curMax = Math.max(nums[i], curMax + nums[i]);
+        max = Math.max(max, curMax);
+    }
+    return max;
+};
+
+console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]));// 6
+console.log(maxSubArray([1]));// 1
+console.log(maxSubArray([5, 4, -1, 7, 8]));// 23
+
+// https://www.youtube.com/watch?v=5WZl3MMT0Eg&t=29s
