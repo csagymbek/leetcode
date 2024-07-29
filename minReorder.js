64e71cf7687bf19a7aa95f14c9760c6964683d62
csagymbek <csagymbek@gmail.com>
2024-07-28 22:48:46 -0700

    initial coomit

--- minReorder.js
+++ minReorder.js
@@ -8,7 +8,29 @@
 // It's guaranteed that each city can reach city 0 after reorder.
 
 var minReorder = function (n, connections) {
-
+    const graph = Array.from({ length: n }, () => []);
+    const set = new Set();
+    let count = 0;
+    for (let [a, b] of connections) {
+        graph[a].push(b);
+        graph[b].push(a);
+        set.add(a + "," + b);
+    }
+    function dfs(node, parent) {
+        if (set.has(parent + "," + node)) {
+            count++;
+        }
+        for (let n of graph[node]) {
+            if (n === parent) {
+                continue;
+            }
+            dfs(n, node);
+        }
+    }
+    dfs(0, -1);
+    return count;
 };
 
-console.log(minReorder())
+console.log(minReorder(6, [[0, 1], [1, 3], [2, 3], [4, 0], [4, 5]]));// 3
+console.log(minReorder(5, [[1, 0], [1, 2], [3, 2], [3, 4]]));// 2
+console.log(minReorder(3, [[1, 0], [2, 0]]));// 0
